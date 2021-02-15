048d469cfb40e1c74b066bd9d18324de1dd5f27f
richardhenyash <richardash1@gmail.com>
2021-02-15 09:45:00 +0000

    Bug fix for GitHub page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,7 +38,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -71,3 +72,4 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+$(document).ready(fetchGitHubInformation);
